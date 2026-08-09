59a0d15b2c30c2ba4db88ab50e6252a995d52b3c
SvenskaXCVI <SvenskaXCVI@users.noreply.github.com>
2026-08-09 13:55:12 -0700

    Sync school calendar events across devices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='daylight-mobile-v7';
+const CACHE='daylight-mobile-v8';
 const CORE=['./','./index.html','./styles.css','./app.js','./seed-data.js','./manifest.webmanifest','./icons/daylight-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
