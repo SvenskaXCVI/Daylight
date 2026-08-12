f43b2af0361e63a64bdd68e1208ef37b316635be
SvenskaXCVI <SvenskaXCVI@users.noreply.github.com>
2026-08-12 10:33:16 -0700

    Keep refresh animation below Dynamic Island

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='daylight-mobile-v31';
+const CACHE='daylight-mobile-v32';
 const CORE=['./','./index.html','./styles.css','./app.js','./seed-data.js','./manifest.webmanifest','./icons/daylight-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
