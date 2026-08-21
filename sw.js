bf43e9acf6878ebd31a856de7fde53b6d5f8722d
SvenskaXCVI <SvenskaXCVI@users.noreply.github.com>
2026-08-21 06:27:48 -0700

    Lock mobile floating actions into alignment

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='daylight-mobile-v40';
+const CACHE='daylight-mobile-v41';
 const CORE=['./','./index.html','./styles.css','./app.js','./seed-data.js','./manifest.webmanifest','./icons/daylight-arc-icon.png','./icons/daylight-assistant-action-black.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
